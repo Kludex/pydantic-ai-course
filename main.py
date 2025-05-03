6065278e427c035d44279c56057232d0bf061b24
Marcelo Trylesinski <marcelotryle@gmail.com>
2025-05-03 10:32:22 +0200

    Add second video

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
+import os
 from pydantic_ai import Agent
+import httpx
+from datetime import datetime
 import logfire
 
 logfire.configure()
 logfire.instrument_pydantic_ai()
 logfire.instrument_httpx(capture_all=True)
 
+open_weather_api_key = os.getenv("OPEN_WEATHER_API_KEY")
+assert open_weather_api_key is not None, "OPEN_WEATHER_API_KEY is not set"
 
-agent = Agent("openai:gpt-4.1-nano")
+agent = Agent("openai:gpt-4.1")
+client = httpx.Client()
 
-result = agent.run_sync("Ola, tudo bem?")
+
+@agent.tool_plain
+def get_current_date():
+    return datetime.now().strftime("%Y-%m-%d")
+
+
+@agent.tool_plain
+def get_temperature(lat: float, lon: float) -> float:
+    """Get the temperature in a given location.
+
+    Args:
+        lat: The latitude of the location.
+        lon: The longitude of the location.
+    """
+    # ❯ http "https://api.openweathermap.org/data/2.5/weather?lat=23.5558&lon=46.6396&units=metric&appid=$OPEN_WEATHER_API_KEY"
+    response = client.get(
+        "https://api.openweathermap.org/data/2.5/weather",
+        params={
+            "lat": lat,
+            "lon": lon,
+            "units": "metric",
+            "appid": open_weather_api_key,
+        },
+    )
+    response.raise_for_status()
+    data = response.json()
+    return data["main"]["temp"]
+
+
+result = agent.run_sync("Qual a temperatura em São Paulo?")
 print(result.output)
